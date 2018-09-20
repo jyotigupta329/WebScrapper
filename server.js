22a3ba36c128b5c1d3623b551d37480e46f6adaa
jyoti <jyoti.gupta329@gmail.com>
2018-09-20 01:44:30 -0500

    port changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 8080;
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
@@ -33,15 +33,15 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webScrapper";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webScrapper";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+// // Set mongoose to leverage built in JavaScript ES6 Promises
+// // Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI);
 
 // Connect to the Mongo DB webScrapper
-// mongoose.connect("mongodb://localhost/webScrapper", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/webScrapper", { useNewUrlParser: true });
 
 // Routes
 
